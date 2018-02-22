3d701220aaed31b29d226537cc3ee716ba590b8d
Patrick Meyer <tbt_paddik@hotmail.com>
2018-02-22 16:13:14 +0100

    Add project files.

---
+++ ch02/exercise02/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("It's fun to write my own program!");
+	return 0;
+}
---
+++ ch02/exercise03/main.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main()
+{
+	printf("Howdy, neighbour!\nThis is my first C program.\n");
+	
+	return 0;
+}
