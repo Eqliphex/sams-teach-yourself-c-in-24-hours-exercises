b212e15a913011d1d064087941a16b21a2b0f888
Patrick Meyer <tbt_paddik@hotmail.com>
2018-03-06 15:10:00 +0100

    - updated Exercise8.1

--- ch08/Exercise8.1_Bitwise_Operator/main.c
+++ ch08/Exercise8.1_Bitwise_Operator/main.c
@@ -17,18 +17,19 @@ Using compliment operators.
 	0x1000 = 0001 0000 0000 0000
 	~y = 1110 1111 1111 1111
 
+	signed/unsigned - Leftmost bit taken as an indicator (plus = 0) (minus = 1) effectivly halving the upper capacity from +65,535 to +32,767/-32,767 
 */
 
 int main()
 {
-	short int x = 0, y = 0; // 16 bits(4 bytes) instead of 32 bits (8 bytes) integer
+	unsigned short x = 0, y = 0; // 16 bits(4 bytes) instead of 32 bits (8 bytes) integer
 	x = 0xEFFF;
 	y = 0x1000;
 
 	printf("Given the values: x = 0X%04X, binary: 1110 1111 1111 1111\n	      and y = 0X%04X, binary: 0001 0000 0000 0000\n\n", x, y);
 	printf("The bitwise compliment \"~\", which reverses each bit, is:\n");
-	printf("Expected value for ~x: 0X1000 --- Actual value: 0X%04X\n", ~x);
-	printf("Expected value for ~y: 0XEFFF --- Actual value: 0X%04X", ~y);
+	printf("Expected value for ~x: 0X1000 --- Actual value: 0X%04X\n", (unsigned short)~x);
+	printf("Expected value for ~y: 0XEFFF --- Actual value: 0X%04X", (unsigned short)~y);
 
 	getchar();
 	return SUCCESS;
