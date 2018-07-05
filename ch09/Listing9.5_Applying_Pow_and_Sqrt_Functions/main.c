3af01edb4292f6db472553b07d42a602ff33cf3a
eqliphex <tbt_paddik@hotmail.com>
2018-07-05 13:11:00 +0200

    - Update

---
+++ ch09/Listing9.5_Applying_Pow_and_Sqrt_Functions/main.c
@@ -0,0 +1,29 @@
+﻿/* 09L05: Using pow(), and sqrt() math functions */
+#define SUCCESS 0
+
+#include <stdio.h>
+#include <math.h>
+
+
+int main()
+{
+	double x, y, z;
+
+	x = 64.0;
+	y = 3.0;
+	z = 0.5;
+
+	printf("pow(64.0, 3.0) returns: %7.0f\n", pow(x, y));
+	printf("sqrt(64.0) returns:     %2.0f\n", sqrt(x));
+	printf("pow(64.0, 0.5) returns: %2.0f\n", pow(x, z));
+
+	return SUCCESS;
+}
+
+/*
+* Output:
+*
+* → pow(64.0, 3.0) returns: 262144
+* → sqrt(64.0) returns: 8
+* → pow(64.0, 0.5) returns: 8
+*/
