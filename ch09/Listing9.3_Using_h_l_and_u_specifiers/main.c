18c464274203afed6f8adab3eb6d0e73256a05ab
eqliphex <tbt_paddik@hotmail.com>
2018-07-05 10:46:20 +0200

    s

--- ch09/Listing9.3_Using_h_l_and_u_specifiers/main.c
+++ ch09/Listing9.3_Using_h_l_and_u_specifiers/main.c
@@ -39,7 +39,8 @@ int main()
  *
  * Explanation:
  *									                                                  |  Byte  |  Byte  |    |  Byte  |  Byte  |
- * Line 24 - Since a signed int can hold values ranging from -32.767 .. +32.767 <=> 0b1000 0000 0000 0000 to 0111 1111 1111 1111, where presence of the most significant (i.e leftmost) bit shows if number is negative or not, 
+ * Line 24 - Since a signed int can hold values ranging from -32.767 .. +32.767 <=> 0b1000 0000 0000 0000 to 0111 1111 1111 1111, 
+ *           where presence of the most significant (i.e leftmost) bit shows if number is negative or not, 
  *           and 0xFFFF is hex for (16^4 - 1) <=> (2^16 - 1) = 65.535 = 0b1111 1111 1111 1111, 
  *			 will the signed short int overflow, when it reaches above 0b0111 1111 1111 1111 (32.767), and count up from -32.767 until reaching -1.
  *			 IMPORTANT: it's (65.536 - 1) because we start counting from 0, 1, 2... and not 1, 2, 3... i.e zero-based counting. 
