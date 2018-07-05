f831777d265baba9df1cda93f178700f0c0ae1ba
eqliphex <tbt_paddik@hotmail.com>
2018-07-05 12:29:37 +0200

    - Added C09L04, Math

---
+++ ch09/Listing9.4_Using_Cos_Sin_and_Tan/main.c
@@ -0,0 +1,31 @@
+﻿/* 09L04: Using sin(), cos, and tan() math functions */
+#define _USE_MATH_DEFINES /*Flag defined in math.h preprocessor that we want access to defined values.*/
+#define SUCCESS 0
+
+#include<stdio.h>
+#include <math.h>
+
+
+int main()
+{
+	double doubleValue;
+
+	double degree = 45.0;    
+	
+
+	degree *= M_PI / 180; // Convert to radians
+
+	printf("The sine of 45 is:     %f.\n", sin(degree));
+	printf("The cosine of 45 is:   %f.\n", cos(degree));
+	printf("The tangent of 45 is:  %f.\n", tan(degree));
+
+	return SUCCESS;
+}
+
+/*
+* Output:
+*
+* → The sine of 45 is: 0.707107.
+* → The cosine of 45 is: 0.707107.
+* → The tangent of 45 is: 1.000000.
+*/
