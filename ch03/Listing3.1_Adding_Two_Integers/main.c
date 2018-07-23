28f0f594be536a4bc9305af19b83f3780172cd03
eqliphex <tbt_paddik@hotmail.com>
2018-07-23 16:55:34 +0200

    - Added listing 3.1

---
+++ ch03/Listing3.1_Adding_Two_Integers/main.c
@@ -0,0 +1,30 @@
+﻿/* 03L01:
+* Function to add two integers and return the result.
+*/
+
+#define SUCCESS 0
+
+#include<stdio.h>
+#include "main.h"
+
+
+int summation_two_integers(const int first_factor, const int second_factor)
+{
+	return first_factor + second_factor;
+}
+
+
+int main()
+{
+	int const first_integer = 2;
+	int const second_integer = 3;
+	printf("The sum of, %d and %d, is: %d\n", first_integer, second_integer, summation_two_integers(first_integer, second_integer));
+
+	return SUCCESS;
+}
+
+/*
+* Output:
+*
+* → The sum of, 2 and 3, is: 5
+*/
---
+++ ch03/Listing3.1_Adding_Two_Integers/main.h
@@ -0,0 +1,9 @@
+#pragma once
+
+/**
+ * \brief Method to take the two factor parameters, and return the sum of the two.
+ * \param first_factor 
+ * \param second_factor 
+ * \return 
+ */
+int summation_two_integers(int first_factor, int second_factor);
